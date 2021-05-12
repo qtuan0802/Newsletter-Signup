96fd7fd6187a241040b45b6e2580affaf68209a0
TUAN <qtuan0802@gmail.com>
2021-05-12 12:38:31 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+
+app.use(express.static(__dirname));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+app.post("/failure", function (req, res) {
+  res.redirect("/");
+});
+app.post("/", function (req, res) {
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
+  const https = require("https");
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: { FNAME: firstName, LNAME: lastName },
+      },
+    ],
+  };
+  const jsonData = JSON.stringify(data);
+  const url = "https://us1.api.mailchimp.com/3.0/lists/7a312fc6e";
+  const options = {
+    method: "POST",
+    auth: "TuanNgo1:5a2027319467baadda44e87f91fbe99f-us1",
+  };
+
+  const request = https.request(url, options, function (response) {
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    });
+  });
+  request.write(jsonData);
+  request.end();
+});
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Server start at port 3000");
+});
+
+// API key
+// 5a2027319467baadda44e87f91fbe99f-us1
+
+// List ID
+// 7a312fc6e9
